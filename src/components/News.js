5cb948a9536d42920805adec9939b1a7f127c4ef
Moon Patel <moonpatel2003@gmail.com>
2022-10-04 10:18:12 +0530

    news api fetching

--- src/components/News.js
+++ src/components/News.js
@@ -2,50 +2,45 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
-    articles = [
-        {
-            source: { id: "the-wall-street-journal", name: "The Wall Street Journal" },
-            author: "Joseph De Avila and Alyssa Lukpat",
-            title: "Adderall Shortages Push ADHD Patients to Make Adjustments - The Wall Street Journal",
-            description:
-                "A labor shortage at a pharmaceutical company along with increased demand left Adderall on back-order at many pharmacies across the country.",
-            url: "https://www.wsj.com/articles/adderall-shortages-push-adhd-patients-to-make-adjustments-11663362272",
-            urlToImage: "https://images.wsj.net/im-624803/social",
-            publishedAt: "2022-09-16T21:04:00Z",
-            content:
-                "Patients with attention-deficit hyperactivity disorder are having trouble filling prescriptions of Adderall and generic versions of the drug\r\n amid growing demand and a shortage of the medication, ac… [+415 chars]",
-        },
-        {
-            source: { id: "the-wall-street-journal", name: "The Wall Street Journal" },
-            author: "Joseph Walker",
-            title: "Crispr Gene-Editing Drugs Show Promise in Preliminary Study - The Wall Street Journal",
-            description:
-                "Intellia Therapeutics said two of its experimental Crispr treatments produced positive results in early-stage trials.",
-            url: "https://www.wsj.com/articles/crispr-gene-editing-drugs-show-promise-in-preliminary-study-11663327063",
-            urlToImage: "https://images.wsj.net/im-624113/social",
-            publishedAt: "2022-09-16T21:03:00Z",
-            content:
-                "Intellia Therapeutics Inc. reported encouraging early-stage study results for its Crispr gene-editing treatments, the latest sign that \r\nthe pathbreaking technology could result in commercially avail… [+266 chars]",
-        },
-        {
-            source: { id: "cnn", name: "CNN" },
-            author: "Sean Lyngaas, CNN",
-            title: "Uber investigating 'cybersecurity incident' after hacker claims to access internal systems - CNN",
-            description:
-                'Uber said Thursday that it was investigating a "cybersecurity incident" after a hacker shared evidence that they had breached the ride-hailing giant\'s computer systems with journalists and security researchers.',
-            url: "https://www.cnn.com/2022/09/15/tech/uber-cybersecurity-incident/index.html",
-            urlToImage: "https://cdn.cnn.com/cnnnext/dam/assets/220915222040-02-uber-file-super-tease.jpg",
-            publishedAt: "2022-09-16T20:54:00Z",
-            content: null,
-        },
-    ];
+    // we construct an empty News component using contructor
     constructor(props) {
         super(props);
-        console.log("Contructor of News component");
+        // set state for the News
         this.state = {
-            articles: this.articles,
+            articles: [],
             loading: false,
+            page: 1,
+            totalArticles: 0,
         };
+        this.handleNextClick = this.handleNextClick.bind(this)
+    }
+    // executes after component gets rendered on the screen
+    async componentDidMount() {
+        let pageSize = 5;
+        const url = `https://newsapi.org/v2/top-headlines?country=in&from=2022-09-18&apiKey=d69c4264e10745f6bb5bcf51c05b4561&pageSize=${pageSize}&page=${this.state.page}`;
+        let news = await fetch(url);
+        let parsedNews = await news.json();
+        // console.log(parsedNews.articles);
+        this.setState({ articles: parsedNews.articles, pageSize: pageSize, totalArticles: parsedNews.totalResults });
+    }
+    handlePreviousClick = async () => {
+        console.log('previous')
+        const url = `https://newsapi.org/v2/top-headlines?country=in&from=2022-09-18&apiKey=d69c4264e10745f6bb5bcf51c05b4561&pageSize=${
+            this.state.pageSize
+        }&page=${this.state.page - 1}`;
+        let news = await fetch(url);
+        let parsedNews = await news.json();
+        console.log(parsedNews.articles);
+        this.setState({ articles: parsedNews.articles, page: this.state.page - 1 });
+    }
+    handleNextClick = async () => {
+        console.log('next')
+        console.log(this.state)
+        const url = `https://newsapi.org/v2/top-headlines?country=in&from=2022-09-18&apiKey=d69c4264e10745f6bb5bcf51c05b4561&pageSize=${this.state.pageSize}&page=${this.state.page + 1}`;
+        let news = await fetch(url);
+        let parsedNews = await news.json();
+        console.log(parsedNews.articles);
+        this.setState({ articles: parsedNews.articles, page: this.state.page + 1 });
     }
     render() {
         return (
@@ -55,31 +50,33 @@ export default class News extends Component {
                     {this.state.articles.map((elem) => {
                         return (
                             <div key={elem.url} className="col-md-4">
-                                <NewsItem title={elem.title} description={elem.description} imgURL={elem.urlToImage} newsURL={elem.url} />
+                                <NewsItem
+                                    title={elem.title}
+                                    description={elem.description}
+                                    imgURL={elem.urlToImage}
+                                    newsURL={elem.url}
+                                />
                             </div>
                         );
                     })}
-                    {/* <div className="col-md-4">
-                        <NewsItem
-                            title="Billionaire Ray Dalio warns stocks could plunge 20% if interest rates rise to 4.5% - Fox Business"
-                            description="Ray Dalio, the founder of Bridgewater Associates, said in a post this week that interest rates will need to rise substantially in order to fight inflation."
-                            imgURL="https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2021/09/0/0/GettyImages-1140767704.jpg?ve=1&tl=1"
-                        />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem
-                            title="Billionaire Ray Dalio warns stocks could plunge 20% if interest rates rise to 4.5% - Fox Business"
-                            description="Ray Dalio, the founder of Bridgewater Associates, said in a post this week that interest rates will need to rise substantially in order to fight inflation."
-                            imgURL="https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2021/09/0/0/GettyImages-1140767704.jpg?ve=1&tl=1"
-                        />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem
-                            title="Billionaire Ray Dalio warns stocks could plunge 20% if interest rates rise to 4.5% - Fox Business"
-                            description="Ray Dalio, the founder of Bridgewater Associates, said in a post this week that interest rates will need to rise substantially in order to fight inflation."
-                            imgURL="https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2021/09/0/0/GettyImages-1140767704.jpg?ve=1&tl=1"
-                        />
-                    </div> */}
+                </div>
+                <div className="d-flex justify-content-between">
+                    <button
+                        hidden={this.state.page <= 1 ? true : false}
+                        className="btn btn-warning"
+                        onClick={this.handlePreviousClick}
+                    >
+                        Previous
+                    </button>
+                    <button
+                        hidden={
+                            this.state.page >= Math.ceil(this.state.totalArticles / this.state.pageSize) ? true : false
+                        }
+                        className="btn btn-warning"
+                        onClick={this.handleNextClick}
+                    >
+                        Next
+                    </button>
                 </div>
             </div>
         );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 
 export default class NewsItem extends Component {
+    // constructor(props) {
+    //     super(props)
+    // }
     render() {
         const { title, description, newsURL, imgURL } = this.props;
         return (
-            <div className="card">
-                <img src={imgURL} className="card-img-top" alt="thumbnail" />
+            <div className="card my-3">
+                <img src={imgURL?imgURL:"https://redzonekickboxing.com/wp-content/uploads/2017/04/default-image.jpg"} className="card-img-top" alt="thumbnail" />
                 <div className="card-body">
-                    <h5 className="card-title">{title.slice(0, 50)}...</h5>
-                    <p className="card-text">{description.slice(0, 120)}...</p>
-                    <a href={newsURL} target="_blank" className="btn btn-sm btn-warning">
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">{description}...</p>
+                    <a href={newsURL} rel="noreferrer" target="_blank" className="btn btn-sm btn-warning">
                         Read More
                     </a>
                 </div>
