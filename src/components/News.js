f35b7a923bcbc261f8ea2190cfee36ff711f28f3
Moon Patel <moonpatel2003@gmail.com>
2022-10-11 15:38:43 +0530

    category-wise news fetching

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export default class News extends Component {
     // we construct an empty News component using contructor
@@ -14,52 +15,65 @@ export default class News extends Component {
         };
         this.handleNextClick = this.handleNextClick.bind(this);
     }
+
     // executes after component gets rendered on the screen
-    async componentDidMount() {
+    componentDidMount = async () => {
         let pageSize = 6;
-        const url = `https://newsapi.org/v2/top-headlines?country=in&from=2022-09-18&apiKey=d69c4264e10745f6bb5bcf51c05b4561&pageSize=${pageSize}&page=${this.state.page}`;
-        this.setState({loading:true})
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d69c4264e10745f6bb5bcf51c05b4561&pageSize=${pageSize}&page=${this.state.page}`;
+        this.setState({ loading: true });
+        console.log(url)
         let news = await fetch(url);
         let parsedNews = await news.json();
-        this.setState({ articles: parsedNews.articles, pageSize: pageSize, totalArticles: parsedNews.totalResults, loading:false });
-    }
+        this.setState({
+            articles: parsedNews.articles,
+            pageSize: pageSize,
+            totalArticles: parsedNews.totalResults,
+            loading: false,
+        });
+    };
     handlePreviousClick = async () => {
-        const url = `https://newsapi.org/v2/top-headlines?country=in&from=2022-09-18&apiKey=d69c4264e10745f6bb5bcf51c05b4561&pageSize=${
-            this.state.pageSize
-        }&page=${this.state.page - 1}`;
-        this.setState({loading:true})
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${
+            this.props.category
+        }&apiKey=d69c4264e10745f6bb5bcf51c05b4561&pageSize=${this.state.pageSize}&page=${
+            this.state.page - 1
+        }`;
+        this.setState({ loading: true }); // show spinner before fetching data
         let news = await fetch(url);
         let parsedNews = await news.json();
-        this.setState({ articles: parsedNews.articles, page: this.state.page - 1, loading:false });
+        this.setState({ articles: parsedNews.articles, page: this.state.page - 1, loading: false });
     };
     handleNextClick = async () => {
         console.log(this.state);
-        const url = `https://newsapi.org/v2/top-headlines?country=in&from=2022-09-18&apiKey=d69c4264e10745f6bb5bcf51c05b4561&pageSize=${
-            this.state.pageSize
-        }&page=${this.state.page + 1}`;
-        this.setState({loading:true})
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${
+            this.props.category
+        }&apiKey=d69c4264e10745f6bb5bcf51c05b4561&pageSize=${this.state.pageSize}&page=${
+            this.state.page + 1
+        }`;
+        this.setState({ loading: true }); // show spinner before fetching data
         let news = await fetch(url);
         let parsedNews = await news.json();
-        this.setState({ articles: parsedNews.articles, page: this.state.page + 1, loading:false });
+        this.setState({ articles: parsedNews.articles, page: this.state.page + 1, loading: false });
     };
     render() {
         return (
             <div className="container my-3">
                 <h2 className="my-3">Top Headlines</h2>
-                {this.state.loading && <Spinner/>}
+                {/* show spinner when this.state.loading is true */}
+                {this.state.loading && <Spinner />}
                 <div className="row">
-                    {this.state.articles.map((elem) => {
-                        return (
-                            <div key={elem.url} className="col-md-4">
-                                <NewsItem
-                                    title={elem.title}
-                                    description={elem.description}
-                                    imgURL={elem.urlToImage}
-                                    newsURL={elem.url}
-                                />
-                            </div>
-                        );
-                    })}
+                    {!this.state.loading &&
+                        this.state.articles.map((elem) => {
+                            return (
+                                <div key={elem.url} className="col-md-4">
+                                    <NewsItem
+                                        title={elem.title}
+                                        description={elem.description}
+                                        imgURL={elem.urlToImage}
+                                        newsURL={elem.url}
+                                    />
+                                </div>
+                            );
+                        })}
                 </div>
                 <div className="d-flex justify-content-between">
                     <button
@@ -83,3 +97,14 @@ export default class News extends Component {
         );
     }
 }
+
+News.defaultProps = {
+    country: "in",
+    pageSize: 6,
+    category: "general",
+};
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+};
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,8 +7,8 @@ export default class NewsItem extends Component {
             <div className="card my-3">
                 <img src={imgURL?imgURL:"https://redzonekickboxing.com/wp-content/uploads/2017/04/default-image.jpg"} className="card-img-top" alt="thumbnail" />
                 <div className="card-body">
-                    <h5 data-bs-toggle="tooltip" data-bs-title="Default tooltip" className="card-title">{title.slice(0,120)}...</h5>
-                    <p className="card-text">{description.slice(0,150)}...</p>
+                    <h5 className="card-title">{title?title.slice(0,120):"null"}...</h5>
+                    <p className="card-text">{description?description.slice(0,150):"null"}...</p>
                     <a href={newsURL} rel="noreferrer" target="_blank" className="btn btn-sm btn-warning">
                         Read More
                     </a>
